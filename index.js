4e55d84642697167be8afef57f3a6e12c56416eb
fragianci <francescogiancipoli15@gmail.com>
2025-04-16 16:18:54 +0200

    Refactor API to connect to MongoDB and retrieve movie data. Updated schema properties and added CORS support. Changed endpoint to /api/movies with aggregation pipeline for data retrieval.

--- index.js
+++ index.js
@@ -13,18 +13,61 @@
  *               items:
  *                 type: object
  *                 properties:
- *                   id:
+ *                   _id:
+ *                     type: string
+ *                   genres:
+ *                     type: array
+ *                     items:
+ *                       type: string
+ *                   title:
+ *                     type: string
+ *                   num_mflix_comments:
  *                     type: integer
- *                   name:
+ *                   poster:
+ *                     type: string
+ *                   fullplot:
+ *                     type: string
+ *                   cast:
+ *                     type: array
+ *                     items:
+ *                       type: string
+ *                   year:
+ *                     type: integer
+ *                   type:
  *                     type: string
  */
-
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
 
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = 3000;
+
+// Abilita CORS
+app.use(cors());
+
+// per prod
+// app.use(
+//   cors({
+//     origin: "https://fragianci.github.io/anime-angular/",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+// Connessione a MongoDB
+const uri =
+  "mongodb+srv://mongo:test@cluster0.zhjh5zf.mongodb.net/test?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send(`
@@ -33,20 +76,48 @@ app.get("/", (req, res) => {
   `);
 });
 
-// Rotta API
-app.get("/anime", (req, res) => {
-  res.json([
-    { id: 1, name: "Naruto" },
-    { id: 2, name: "One Piece" },
-    { id: 3, name: "Attack on Titan" },
-  ]);
+app.get("/api/movies", async (req, res) => {
+  try {
+    await client.connect();
+    const collection = client.db("sample_mflix").collection("movies");
+    const pipeline = [
+      {
+        $match: {
+          runtime: { $gt: 15 },
+          num_mflix_comments: { $lt: 110 },
+          type: "movie",
+        },
+      },
+      { $sort: { year: -1 } },
+      {
+        $project: {
+          name: 1,
+          genres: 1,
+          title: 1,
+          num_mflix_comments: 1,
+          poster: 1,
+          fullplot: 1,
+          cast: 1,
+          year: 1,
+          type: 1,
+        },
+      },
+      { $limit: 20 },
+    ];
+
+    const movies = await collection.aggregate(pipeline).toArray();
+    res.json(movies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Errore nel recupero dei dati");
+  } finally {
+    await client.close();
+  }
 });
 
 // Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Avvio server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-  console.log(`Swagger docs at http://localhost:${port}/api-docs`);
+  console.log(`Server in ascolto su http://localhost:${port}`);
 });
